9d99dbe4980d4775b240c6666a22a8861437b339
KamilB38 <kamil.bilgin@gmail.com>
2015-01-20 12:48:54 +0100

    functions for lexical scoping assingnment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do ......
+#example for running the code:
+#mymatrix <- matrix(6:9,2,2)
+#solvelist <- makeCacheMatrix(mymatrix)
+#cacheSolve(solvelist)
 
-## Write a short comment describing this function
 
+#argument is a matrix
+# returns a list
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(invs) inv <<- invs
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+#argument is a list
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data) %*% data #inverse the matrix
+  x$setinv(inv)
+  inv
 }
